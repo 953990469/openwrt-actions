cd2472b478fc51f14574968e7255adc7a3be3da0
hyird <hyird@outlook.com>
2020-08-08 13:47:29 +0800

    x

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Openwrt-actions
+# Action-Openwrt
+![](https://github.com/hyird/Action-Openwrt/workflows/Openwrt-AutoBuild/badge.svg)
+![](https://img.shields.io/github/downloads/hyird/Action-Openwrt/total)
+![](https://img.shields.io/github/v/release/hyird/Action-Openwrt)
---
+++ user/lienol-x64-master/custom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Test custom.sh"
+
+version=`date "+%m.%d"`
+sed -i '92d'                                                                   package/system/opkg/Makefile
+sed -i '/lienol/d'                                                             package/default-settings/files/zzz-default-settings
+sed -i '/shadow/d'                                                             package/default-settings/files/zzz-default-settings
+sed -i "s/#sed/sed/g"                                                          package/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"                        package/default-settings/files/zzz-default-settings
+sed -i "s/https/L20.$version/g"                                                package/default-settings/files/zzz-default-settings
+sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'                                 package/default-settings/files/zzz-default-settings
+sed -i '/exit/d'                                                               package/default-settings/files/zzz-default-settings
+echo "sed -i \"s/19.07-SNAPSHOT/L20.$version/g\" /etc/openwrt_release " >>     package/default-settings/files/zzz-default-settings
+echo "exit 0" >>                                                               package/default-settings/files/zzz-default-settings                   
+
