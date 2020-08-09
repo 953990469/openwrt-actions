fd3abde5448ff5db6b41dd26cef6a7008f59028d
hyird <31695447+hyird@users.noreply.github.com>
2020-08-09 23:22:07 +0800

    Update custom.sh

--- user/lienol-x64-master/custom.sh
+++ user/lienol-x64-master/custom.sh
@@ -8,8 +8,8 @@ sed -i '92d'                                                                   p
 sed -i '/lienol/d'                                                             package/default-settings/files/zzz-default-settings
 sed -i '/shadow/d'                                                             package/default-settings/files/zzz-default-settings
 sed -i "s/#sed/sed/g"                                                          package/default-settings/files/zzz-default-settings
-sed -i "s/openwrt.proxy.ustclug.org/raw.githubusercontent.com\/hyird\/openwrt-actions\/$1\/L20.$version/g"                        package/default-settings/files/zzz-default-settings
-sed -i "s/https/L20.$version/g"                                                package/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"                            package/default-settings/files/zzz-default-settings
+sed -i "s/https/L20.8.6/g"                                                package/default-settings/files/zzz-default-settings
 sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'                                 package/default-settings/files/zzz-default-settings
 sed -i '/exit/d'                                                               package/default-settings/files/zzz-default-settings
 echo "sed -i \"s/19.07-SNAPSHOT/L20.$version/g\" /etc/openwrt_release " >>     package/default-settings/files/zzz-default-settings
